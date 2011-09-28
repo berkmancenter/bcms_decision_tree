50480e1cedfbc168ebbb8aa298ffd4816d106f32
Anita <anita@anitapatel.com>
2011-09-28 15:37:08 -0400

    validating uniqueness of node name

--- app/models/node.rb
+++ app/models/node.rb
@@ -1,6 +1,7 @@
 class Node < ActiveRecord::Base
   acts_as_content_block
   has_and_belongs_to_many :choices, :class_name => 'Node', :foreign_key => 'choice_id', :join_table => 'choices_nodes'
+  validates_uniqueness_of :name
   
   def is_question?
     self.choices.length > 1
